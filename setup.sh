924ed31cb740022d92faab94ca6228aeab318440
Gordon <GordonAccount@yahoo.com>
2025-05-29 17:40:59 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pip3 install -r requirements.txt
 
 echo "Downloading Mars terrain data... (≈ 11GB)"
 echo "This may take a while..."
-curl -o Mars_HRSC_MOLA_BlendDEM_Global_200mp_v2.tif https://planetarymaps.usgs.gov/mosaic/Mars/HRSC_MOLA_Blend/Mars_HRSC_MOLA_BlendDEM_Global_200mp_v2.tif
+curl -o Mars_HRSC_MOLA_BlendDEM_Global_200mp_v2.tif https://asc-pds-services.s3.us-west-2.amazonaws.com/mosaic/Mars/HRSC_MOLA_Blend/Mars_HRSC_MOLA_BlendDEM_Global_200mp_v2.tif
 
 echo "Setup complete. You can now run the simulator with:"
 echo "python3 main.py"
